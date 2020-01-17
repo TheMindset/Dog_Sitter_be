a3f7434b3ad6c970e5c2abf5f92fc18b6a30dfcf
TheMindset <mulezig2014@outlook.fr>
2020-01-17 09:35:25 +0100

    Add UserType

--- app/graphql/types/user_type.rb
+++ app/graphql/types/user_type.rb
@@ -2,5 +2,11 @@
 
 module Types
   class UserType < Types::BaseObject
+    field :id, ID, null: false
+    field :email, String, null: false
+    field :first_name, String, null: false
+    field :last_name, String, null: false
+    field :short_desc, String, null: false
+    field :long_desc, String, null: false
   end
 end
