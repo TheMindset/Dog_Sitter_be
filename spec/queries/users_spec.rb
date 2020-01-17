4e25910810cce555565fde4cab2c132dbef1543f
TheMindset <mulezig2014@outlook.fr>
2020-01-17 11:03:57 +0100

    Add test for dogs field on users query

--- spec/queries/users_spec.rb
+++ spec/queries/users_spec.rb
@@ -6,6 +6,7 @@ require 'pry'
 RSpec.describe 'Users query', type: :request do
   it 'returns a list of users' do
     users = create_list(:user, 5)
+    dogs = create_list(:dog, 3, user: users.first)
 
     post '/graphql', params: { query: query }
     json = JSON.parse(response.body)
@@ -22,6 +23,21 @@ RSpec.describe 'Users query', type: :request do
       'shortDesc' => first_users.short_desc,
       'longDesc' => first_users.long_desc
     )
+
+    user_dogs = data.first['dogs']
+    first_user_dog = user_dogs.first
+
+    expect(user_dogs.count).to eq(3)
+    expect(first_user_dog).to include(
+      'id' => dogs.first.id.to_s,
+      'name' => dogs.first.name,
+      'breed' => dogs.first.breed,
+      'weight' => dogs.first.weight,
+      'birthdate' => dogs.first.birthdate.to_s,
+      'activityLevel' => dogs.first.activity_level,
+      'longDesc' => dogs.first.long_desc,
+      'shortDesc' => dogs.first. short_desc
+    )
   end
 
   def query
@@ -33,6 +49,16 @@ RSpec.describe 'Users query', type: :request do
           lastName
           shortDesc
           longDesc
+          dogs {
+            id
+            name
+            breed
+            weight
+            birthdate
+            activityLevel
+            longDesc
+            shortDesc
+          }
         }
       }
     GQL
