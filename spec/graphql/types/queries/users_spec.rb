1d4449e55ff3e22f202df4c34adac50b1966bcfd
TheMindset <mulezig2014@outlook.fr>
2020-01-23 08:11:28 +0100

    Update users query

--- spec/graphql/types/queries/users_spec.rb
+++ spec/graphql/types/queries/users_spec.rb
@@ -5,30 +5,39 @@ require 'pry'
 
 RSpec.describe 'Users query', type: :request do
   it 'returns a list of users' do
-    users = create_list(:user, 5, )
-    dogs = create_list(:dog, 3, user: users.first)
-    location = create(:location, user: users.first)
+    VCR.use_cassette('query_users_and_create_location') do
+      users = create_list(:user, 5, )
+      dogs = create_list(:dog, 3, user: users.first)
 
-    post '/graphql', params: { query: query }
-    json = JSON.parse(response.body)
-    data = json['data']['users']
+      location = Location.create!(
+        user: users.first,
+        street_address: '52 Rue de la Verrerie',
+        zip_code: 75_003,
+        city: 'Paris',
+        state: 'France'
+      )
 
-    expect(data.count).to eq(5)
+      post '/graphql', params: { query: query }
+      json = JSON.parse(response.body)
+      data = json['data']['users']
 
-    first_db_user = users.first
-    first_gql_user = data.first
+      expect(data.count).to eq(5)
 
-    compare_gql_and_db_users(first_gql_user, first_db_user)
+      first_db_user = users.first
+      first_gql_user = data.first
 
-    first_gql_dog = data.first['dogs']
-    first_db_dog = dogs.first
+      compare_gql_and_db_users(first_gql_user, first_db_user)
 
-    compare_gql_and_db_dogs(first_gql_dog, first_db_dog)
+      first_gql_dog = data.first['dogs']
+      first_db_dog = dogs.first
 
-    first_gql_location = data.first['location']
-    first_db_location = location
+      compare_gql_and_db_dogs(first_gql_dog, first_db_dog)
 
-    compare_gql_and_db_location(first_gql_location, first_db_location)
+      first_gql_location = data.first['location']
+      first_db_location = location
+
+      compare_gql_and_db_location(first_gql_location, first_db_location)
+    end
   end
 
   def query
