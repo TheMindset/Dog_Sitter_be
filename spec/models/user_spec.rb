72e57b4c70189d2d5b91b3d8aed827b97b02a973
TheMindset <mulezig2014@outlook.fr>
2020-01-23 15:28:22 +0100

    Add google_token to users table

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -4,14 +4,19 @@
 #
 # Table name: users
 #
-#  id         :bigint           not null, primary key
-#  email      :string
-#  first_name :string
-#  last_name  :string
-#  long_desc  :text
-#  short_desc :text
-#  created_at :datetime         not null
-#  updated_at :datetime         not null
+#  id           :bigint           not null, primary key
+#  email        :string
+#  first_name   :string
+#  google_token :string
+#  last_name    :string
+#  long_desc    :text
+#  short_desc   :text
+#  created_at   :datetime         not null
+#  updated_at   :datetime         not null
+#
+# Indexes
+#
+#  index_users_on_google_token  (google_token)
 #
 
 require 'rails_helper'
