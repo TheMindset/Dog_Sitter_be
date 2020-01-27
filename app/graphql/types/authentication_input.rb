17f73b6f5fe90c011d0d3c9f73525f45203a4e9b
TheMindset <mulezig2014@outlook.fr>
2020-01-27 18:18:00 +0100

    Add authentication_input type

---
+++ app/graphql/types/authentication_input.rb
@@ -0,0 +1,10 @@
+# frozen_string_literal: true
+
+module Types
+  class AuthenticationInput < BaseInputObject
+    argument :first_name, String, required: true
+    argument :last_name, String, required: true
+    argument :email, String, required: true
+    argument :google_token, String, required: true
+  end
+end
