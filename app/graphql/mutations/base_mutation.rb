ffb22fe7c572e4dfafaf924947f236d0dc8d9808
TheMindset <mulezig2014@outlook.fr>
2020-01-28 09:49:17 +0100

    Handle error : Wrong google_token

--- app/graphql/mutations/base_mutation.rb
+++ app/graphql/mutations/base_mutation.rb
@@ -6,5 +6,9 @@ module Mutations
     field_class Types::BaseField
     input_object_class Types::BaseInputObject
     object_class Types::BaseObject
+
+    def boot_unauthorized_user
+      raise GraphQL::ExecutionError, 'Unauthorized - a valid google_token query parameter is required' unless context[:current_user]
+    end
   end
 end
