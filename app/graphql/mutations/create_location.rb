478b7f7343dce197121be30ac4b7d7853942f0bb
TheMindset <mulezig2014@outlook.fr>
2020-01-30 09:50:36 +0100

    Handle sad path in create_location mutation

--- app/graphql/mutations/create_location.rb
+++ app/graphql/mutations/create_location.rb
@@ -9,12 +9,16 @@ module Mutations
     def resolve(location:)
       boot_unauthorized_user
 
-      location = context[:current_user].create_location(
-        street_address: location[:street_address],
-        city: location[:city],
-        state: location[:state],
-        zip_code: location[:zip_code]
-      )
+      begin
+        location = context[:current_user].create_location(
+          street_address: location[:street_address],
+          city: location[:city],
+          state: location[:state],
+          zip_code: location[:zip_code]
+        )
+      rescue RuntimeError => e
+        raise GraphQL::ExecutionError, e.message
+      end
 
       { location: location }
     end
--- spec/graphql/types/mutations/create_location_spec.rb
+++ spec/graphql/types/mutations/create_location_spec.rb
@@ -7,9 +7,9 @@ RSpec.describe 'CreateLocation', type: :request do
     @user = create(:user)
   end
 
-  describe 'with a valid google_token and lacation informations' do
+  describe 'with a valid google_token and location informations' do
     it 'current user can create a new location' do
-      VCR.use_cassette('create_location_validation/with_valide_inforamtions') do
+      VCR.use_cassette('create_location_mutation/with_valide_informations') do
         location = Location.new(
           street_address: '680 Cours de la Libération',
           zip_code: '33405',
@@ -19,7 +19,7 @@ RSpec.describe 'CreateLocation', type: :request do
 
         params = {
           google_token: @user.google_token,
-          query: valide_location_mutation(location)
+          query: location_mutation(location)
         }
 
         post '/graphql', params: params
@@ -33,7 +33,52 @@ RSpec.describe 'CreateLocation', type: :request do
     end
   end
 
-  def valide_location_mutation(location)
+  describe 'with an invalid google_token and a valid location informations' do
+    it 'user can not create a new location' do
+      location = Location.new(
+        street_address: '680 Cours de la Libération',
+        zip_code: '33405',
+        city: 'Talence',
+        state: 'France'
+      )
+
+      params = {
+        google_token: nil,
+        query: location_mutation(location)
+      }
+
+      post '/graphql', params: params
+
+      json = JSON.parse(response.body)
+      errors = json['errors'].first['message']
+
+      expect(errors).to eq('Unauthorized - a valid google_token query parameter is required')
+    end
+  end
+
+  describe 'with valid google_token and invalid location informations' do
+    it 'user can not create a new location' do
+      VCR.use_cassette('create_location_mutation/with_invalide_location') do
+        location = Location.new(
+          street_address: -5,
+        )
+
+        params = {
+          google_token: @user.google_token,
+          query: location_mutation(location)
+        }
+
+        post '/graphql', params: params
+
+        json = JSON.parse(response.body)
+        gql_location = json['data']['createLocation']['location']
+
+        expect(gql_location).to be_nil
+      end
+    end
+  end
+
+  def location_mutation(location)
     "
     mutation {
       createLocation(input: {
